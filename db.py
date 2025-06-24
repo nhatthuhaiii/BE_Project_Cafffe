64e2c4dfe1165f3586cf0860d3d9a7d648dc741d
nhatthuhaiii <118200593+nhatthuhaiii@users.noreply.github.com>
2025-06-24 11:37:50 +0700

    Update db.py renameconnect

--- db.py
+++ db.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 
 # connection Db
-SQLALCHEMY_DB_URL ="postgresql://postgres:123@localhost:5432/project_caffe"
+SQLALCHEMY_DB_URL ="postgresql://postgres:congnhat@db.wqqrxlmjqbnomzimgopn.supabase.co:5432/postgres
+"
 engine = create_engine(SQLALCHEMY_DB_URL) 
 SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
 Base = declarative_base()
