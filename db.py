bd574bafee450a24ab0044a79f2500123372052c
nhatthuhaiii <118200593+nhatthuhaiii@users.noreply.github.com>
2025-06-24 11:42:06 +0700

    Update db.py

--- db.py
+++ db.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 
 # connection Db
-SQLALCHEMY_DB_URL ="postgresql://postgres:congnhat@db.wqqrxlmjqbnomzimgopn.supabase.co:5432/postgres
-"
+SQLALCHEMY_DB_URL = "postgresql://postgres:congnhat@db.wqqrxlmjqbnomzimgopn.supabase.co:5432/postgres"
 engine = create_engine(SQLALCHEMY_DB_URL) 
 SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
 Base = declarative_base()
